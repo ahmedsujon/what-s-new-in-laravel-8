5d6908dc9980c779bfc5186a5fe3492a881b3bd0
ahmedsujon <ahmedsujon050@gmail.com>
2020-10-23 15:45:18 +0600

    first commit

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -16,14 +16,14 @@ class RedirectIfAuthenticated
      * @param  string|null  ...$guards
      * @return mixed
      */
-    public function handle($request, Closure $next, ...$guards)
+    public function handle($request, Closure $next, $guard = null)
     {
-        $guards = empty($guards) ? [null] : $guards;
+        if ($guard == "admin" && Auth::guard($guard)->check()) {
+            return redirect()->route('admin.dashboard');
+        }
 
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
-            }
+       if (Auth::guard($guard)->check()) {
+            return redirect()->route('home');
         }
 
         return $next($request);
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+// Admin routes
+Route::prefix('admin')->group(function(){
+    Route::get('/', 'Web\Admin\AdminController@index')->name('admin.dashboard');
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/register', 'Auth\AdminRegisterController@showRegisterForm')->name('admin.register');
+    Route::post('/register', 'Auth\AdminRegisterController@register')->name('admin.register.submit');
+});
 
 
-Route::get('/',  'WelcomeController@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
